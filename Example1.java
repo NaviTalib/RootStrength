90db449b22b112337e043729087609f076559c2b
NaviTalib <talibali786.ta@gmail.com>
2026-09-22 09:13:52 +0530

    first program done

---
+++ Example1.java
@@ -0,0 +1,8 @@
+// Variables, data types, operators
+
+public class Example1 {
+    public static void main(String[] args){
+        int mark = 55;
+        System.out.println("Marks:"+ mark);
+    }
+}
