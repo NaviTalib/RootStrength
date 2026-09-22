256ae82d17edbb14ec5ecfb9a420516197fd0359
NaviTalib <talibali786.ta@gmail.com>
2026-09-22 10:04:38 +0530

    varibale and data types basic done

--- Example1.java
+++ Example1.java
@@ -1,8 +1,53 @@
-// Variables, data types, operators
+// Variables, data types
 
 public class Example1 {
     public static void main(String[] args){
-        int mark = 55;
-        System.out.println("Marks:"+ mark);
+        // Primitive data types
+
+        // integer data types
+        byte age = 25;
+        System.out.println(age);
+
+        short students = 2500;
+        System.out.println(students);
+
+        int amount = 100000;
+        System.out.println(amount);
+
+        long population = 1400000000L;
+        System.out.println(population);
+
+        // floating data types
+
+        float price = 99.9f;
+        System.out.println(price);
+
+        double pi = 3.1415926535;
+        System.out.println(pi);
+
+        // character data type
+
+        char grade = 'A';
+        System.out.println(grade);
+
+        // boolean data type
+
+        boolean isStudent = true;
+        System.out.println(isStudent);
+
+        // Non primitive data type
+
+        // string
+
+        String name = "Navi";
+        System.out.println(name);
+
+        // array
+
+        // int[] numbers = new int[5]; // for fixed size array
+        int[] numbers = {1,2,3,4,5};
+        System.out.println(numbers[0]);
+
+
     }
 }
