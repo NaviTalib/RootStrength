dfdaba29a9d72f27ca26fef6b9c9b35c7e9ed522
NaviTalib <talibali786.ta@gmail.com>
2026-09-26 14:50:12 +0530

    array declaration and initialization done

---
+++ CoreSyntax/ArrayBasic.java
@@ -0,0 +1,22 @@
+public class ArrayBasic {
+    public static void main(String[] args){
+
+        // Method A: Two-Step Creation (Declare size first)
+
+        // 1 . declaration and size allocation
+        int[] numbers = new int[3];
+
+        // 2. assign values by index
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+
+        System.out.println("first number: "+ numbers[0]);
+
+        // Method B: Direct Literal Initialization
+
+        String[] students = {"Aman","Rahul","Akash"};
+        System.out.println("Total Students: "+ students.length);
+
+    }
+}
